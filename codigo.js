efe609d89f264489ec7dbed93f509e6da81f00ec
VLShub <164173030+VLShub@users.noreply.github.com>
2024-07-12 10:08:50 +0200

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,19 @@
+let fotosMarcadas = 0;
+
+function marcar(evt) {
+    if(evt.target.dataset.marcada == undefined) {
+        evt.target.style.filter = "sepia()";
+        evt.target.dataset.marcada = true;
+        fotosMarcadas++;
+    } else {
+        evt.target.style.filter = "";
+        evt.target.dataset.marcada = false;
+        fotosMarcadas--;
+    }
+    document.getElementById("marcadas").innerText = fotosMarcadas;
+}
+
+const fotos = document.querySelectorAll("#fotos img")
+for (const foto of fotos) {
+    foto.addEventListener("click", marcar);
+}
